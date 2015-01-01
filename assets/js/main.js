8a6d8b67c8f46e8c1ec92af233f542bef985debf
Radim Daniel Panek <rdpanek@gmail.cz>
2015-01-01 13:37:57 +0100

    Update profile image and title

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,31 +22,6 @@ jQuery(document).ready(function($) {
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
 
-    /* jQuery RSS - https://github.com/sdepold/jquery-rss */
-    $("#rss-feeds").rss(
-
-        //Change this to your own rss feeds
-        "http://rdpanek.postach.io/feed.xml",
-
-        {
-        // how many entries do you want?
-        // default: 4
-        // valid values: any integer
-        limit: 10,
-
-        // outer template for the html transformation
-        // default: "<ul>{entries}</ul>"
-        // valid values: any string
-        layoutTemplate: "<div class='item'>{entries}</div>",
-
-        // inner template for each entry
-        // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-        // valid values: any string
-        entryTemplate: '<p><div class="title"><a href="{url}" target="_blank">{title}</a></div><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>celý článek</a></div></p>'
-
-        }
-    );
-
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "rdpanek", selector: "#ghfeed" });
 
